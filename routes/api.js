7947ff3c918b1ee01fc9d49829b1d263a6064092
Testo12 <erik.malmsten99@gmail.com>
2019-01-09 16:01:41 +0100

    Added read capablities

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var controller = require('../controllers/api');
 
 router.post('/user/register', controller.register);
-
 router.post('/notes/create', controller.create);
+router.get('/notes/', controller.read);
 
 module.exports = router;
