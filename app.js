6498b8e4a601500a906ac178fd1c15d9f7e6bc95
RyuTribal <ivan.sedelkin9@gmail.com>
2019-01-11 14:02:18 +0100

    Added an additional header

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
   
