4ead6d6b1720deb04582b4904c59d720177d9e06
yyIntsuki <intsuki.yz@gmail.com>
2019-01-21 11:11:54 +0100

    fixed an issue where database secret was incorrectly set

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': 'ivanIsNumberOneAndTotallyNotTeamCUE',
+    'secret': 'TeamCUEisNumberOneAndTotallyNotIvan',
     'database': "mongodb://User:passw0rd@ds253104.mlab.com:53104/notey"
 }
